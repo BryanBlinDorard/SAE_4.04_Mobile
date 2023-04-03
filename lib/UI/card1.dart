2c6526d3959f9d9d8e8e5e68abe8299b1128b9f7
BryanBlinDorard <brybrybbd@gmail.com>
2023-04-03 12:05:35 +0200

    feat: changement de l'affichage du Home, plus moderne

--- lib/UI/card1.dart
+++ lib/UI/card1.dart
@@ -14,40 +14,40 @@ class Card1 extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     articles = context.watch<ArticleViewModel>().liste;
-    return ListView.builder(
-        itemCount: articles.length,
-        itemBuilder: (context, index)=> Card(
-          elevation: 5,
-          margin: const EdgeInsets.all(6),
-          child: ListTile(
-            title: Text(articles[index].title),
-            subtitle: Text(articles[index].description.length > 30 ? '${articles[index].description.substring(0, 30)}...' : articles[index].description),
-            onTap: () {
-              Navigator.push(context, MaterialPageRoute(
-                builder: (context) => detail(article: articles[index])
-              ));
-            },
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () {
-                    context.read<ArticleViewModel>().deleteArticle(articles[index].getId);
-                  },
-                ),
-                IconButton(
-                  icon: const Icon(Icons.edit),
-                  onPressed: ()  {
-                    Navigator.push(context, MaterialPageRoute(
-                      builder: (context) => EditTask(articles[index])
-                    ));
-                  },
-                ),
-              ],
+    // créer un gridview builder
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemCount: articles.length,
+      itemBuilder: (context, index) => buildImageCard(context, index),
+    );
+  }
+
+  buildImageCard(BuildContext context, int index) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => detail(article: articles[index])));
+      },
+      child: Card(
+        margin: EdgeInsets.all(10),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Container(
+          margin: EdgeInsets.all(10),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.network(
+              articles[index].image,
+              fit: BoxFit.cover,
             ),
           ),
-        ));
+          )
+        ),
+    );
   }
 }
 
